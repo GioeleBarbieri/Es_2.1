5f560a1b524e0596e24bda79fbfdc408ce0394fb
GioeleBarbieri <gioelebarbieri48@gmail.com>
2024-09-27 20:15:44 +0200

    primo esercizio

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float a, b, media;
+    a = 235;
+    b = 567;
+    media = (a + b) / 2;
+    printf("la media tra i due valori e': %f", media);
+    return 0;
+}
